f8b9323668d9d53b7d54963529a7a76e5e51d316
slt5309616 <347098141@qq.com>
2017-02-04 15:33:57 +0800

    Upload all demo files
    Including build files

---
+++ Assets/Editor/U3dDemoEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class csharpDemoEditor  {
+
+	// Use this for initialization
+	[MenuItem("Assets/MakeAB")]
+    private static void MakeAB() {
+        var obj = Selection.activeObject;
+        string targetPath = Application.dataPath + "/StreamingAssets/" + obj.name + ".assetbundle";
+        if (BuildPipeline.BuildAssetBundle(obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies)) {
+            Debug.Log(obj.name + "资源打包成功");
+        } else {
+            Debug.Log(obj.name + "资源打包失败");
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Editor/csharpDemoEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEditor.VersionControl;
+using System.Collections.Generic;
+using System.IO;
+
+public class U3dDemoEditor  {
+
+	// Use this for initialization
+	[MenuItem("Assets/MakeAB")]
+    private static void MakeAB() {
+        BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
